7634dbf93b50da37a8c8624ab0416ab4db522f85
Ildar Gumerov <Ildar.Gumerov@icl-services.com>
2019-08-27 16:30:35 +0300

    Creat get and post todo requests

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app       = express();
 const port      = process.env.PORT || 8000;
 const Todo      = require('./models/index');
 const todoRoutes    = require('./routes/todos');
+const bodyParser    = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
-app.get("/", function(req, res) {
-    res.send("Hellow from root route");
+app.get ("/", function(req, res) {
+    res.send("Hello from root route");
 });
 
 app.use('/api/todos', todoRoutes);
--- routes/todos.js
+++ routes/todos.js
@@ -4,13 +4,27 @@ const db        = require('../models');
 
 
 router.get('/', (req, res) => {
-    db.Todo.find()
-    .then((todos) => {
-        res.send(todos);
-    })
-    .catch((err) => {
+    db.Todo.find().then((todos) => {
+        res.status(201).json(todos);
+    }).catch((err) => {
         Console.log(err);
     });
+});
+
+router.get('/:todoid', (req, res) => {
+    db.Todo.findById(req.params.todoid).then( (todo) => {
+        res.json(todo);
+    }).catch((err) => {
+        res.send(err);
+    });
+});
+
+router.post('/', (req, res) => {
+    const todo = req.body;
+
+    db.Todo.create(todo)
+    .then( (newTodo) => { res.redirect('/api/todos') })
+    .catch( (err) => { res.send(err) });
 });
 
 module.exports = router;

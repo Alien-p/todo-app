77c246b10007195efe1628bd6c6d0783b2c2544d
Ildar Gumerov <Ildar.Gumerov@icl-services.com>
2019-08-28 19:02:52 +0300

    Add index page

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express   = require('express');
 const app       = express();
 const port      = process.env.PORT || 8000;
-const Todo      = require('./models/index');
 const todoRoutes    = require('./routes/todos');
 const bodyParser    = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
-
-app.get ("/", function(req, res) {
-    res.send("Hello from root route");
-});
-
+app.use( bodyParser.json() );
+app.use( bodyParser.urlencoded({ extended: true }) );
+app.set("view engine", "ejs");
 app.use('/api/todos', todoRoutes);
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+
+app.get("/", function(req, res) {
+    res.render('index.html');
+});
 
 app.listen(port, function() {
     console.log("app listening on port " + port);

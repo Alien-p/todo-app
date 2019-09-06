a7125e52003933daabcb414d0769b61cea8dd350
Ildar Gumerov <Ildar.Gumerov@icl-services.com>
2019-09-06 20:21:06 +0300

    Add todo to list

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,37 @@
 $(document).ready(function () {
     $.getJSON("/api/todos")
-        .then(addToDos);
+        .then(addToDos)
+
+    $('#todoInput').keypress(function (e) { 
+        if (e.which == 13) {
+            createTodo();
+        }
+    });
+    
 });
 
 function addToDos(todos) {
     todos.forEach(todo => {
-        const newTodo = $('<li class="task">' + todo.name + '</li>');
-        $('.list').append(newTodo);
+        addTodo(todo)
     });
+}
+
+function addTodo(todo) {
+    const newTodo = $('<li class="task">' + todo.name + '</li>');
+    if (todo.completed) newTodo.addClass("done");
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+    let newTodoVal = $('#todoInput').val();
+
+    $.post('/api/todos', { name: newTodoVal })
+    .then( function(createdTodo) {
+        addTodo(createdTodo[createdTodo.length-1]);
+        $('#todoInput').val('');
+    })
+    .catch( (err) => {
+        console.log(err);
+    });
+
 }
